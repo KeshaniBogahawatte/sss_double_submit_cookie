e15ad4a940ab91eab5797bc18cd29adddf80e4f0
Keshani Bogahawatte <41075638+KeshaniBogahawatte@users.noreply.github.com>
2019-05-19 13:25:10 +0530

    Update Database.java

--- sss_doublesubmitcookie/src/main/java/Databases/Database.java
+++ sss_doublesubmitcookie/src/main/java/Databases/Database.java
@@ -2,11 +2,12 @@ package Databases;
 
 public class Database
 {
-
+  //the login credentials have been hard coded. username ="admin" and password ="admin"
   private static final String USERNAME = "admin";
 
   private static final String PASSWORD = "admin";
-
+  
+  //check the entered login credentials with the stored login credentials
   public static boolean isValidUser(String username, String password)
   {
     return USERNAME.equalsIgnoreCase(username) && PASSWORD.equalsIgnoreCase(password);
